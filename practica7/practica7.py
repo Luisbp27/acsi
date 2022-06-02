1b8011a1caa6c11485e767f074fb51650c795cd2
Luisbp27 <luisbarcapons@gmail.com>
2022-06-02 19:24:06 +0200

    Faltan las tendencias

--- practica7/practica7.py
+++ practica7/practica7.py
@@ -1,12 +1,13 @@
 import numpy as np
 import statistics
+from pyparsing import line
 from scipy import stats
 import matplotlib.pyplot as plt
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-g", "--group", help="Select group or ungrouped data",
-                    default=0, type=int)
+                    default=1, type=int)
 args = parser.parse_args()
 
 group = args.group
@@ -18,29 +19,23 @@ data = data[8:]
 
 hours = sorted(list({x.split(',')[1] for x in data}))
 
+print(hours)
+
 
 def size():
     plt.figure()
     plt.rcParams.update({"font.family": "serif"})
 
-    x_arr = []
-    y_arr = []
-
     if group == 0:
         x, y = get_axes(0)
-        plt.scatter(x, y)
-        x_arr.append(x)
-        y_arr.append(y)
+        plt.scatter(x, y, s = 3)
         
     else:
-        x_size = []
-
         for hour in hours:
-            x = get_axes_mean(hour, 0)
-            plt.scatter(x, hour)
-            x_size.append(x)
-
-    print(y_arr)
+            y = get_axes_mean(hour, 0)
+            plt.scatter(hour, y, s = 8)
+        
+        lineal_regression(y, "size")
 
     # Plot data of size
     plt.title("Size")
@@ -55,14 +50,13 @@ def velocity():
 
     if group == 0:
         x, y = get_axes(2)
-        plt.scatter(x, y)
+        plt.scatter(x, y, s = 3)
     else:
-        x_velocity = []
-
         for hour in hours:
-            x, y = get_axes_mean(hour, 2)
-            plt.scatter(x, hour)
-            x_velocity.append(x)
+            y = get_axes_mean(hour, 2)
+            plt.scatter(hour, y, s = 8)
+
+        lineal_regression(y, "velocity")
 
     # Plot data of velocity
     plt.title("Velocity")
@@ -72,6 +66,19 @@ def velocity():
 
 def format(x):
     return float(('%.4f' % x).rstrip('0').rstrip('.'))
+
+def get_axes(value):
+    x = []
+    y = []
+
+    for line in data:
+        line = line.split(',')
+    
+        x.append(int(line[1]))
+        y.append(float(line[value]))
+
+    return x, y
+
 
 def get_axes_mean(hour, value):
     x = []
@@ -87,31 +94,14 @@ def get_axes_mean(hour, value):
         return format(statistics.harmonic_mean(x))
 
 
-def get_axes(value):
-    x = []
-    y = []
-
-    for line in data:
-        line = line.split(',')
-        x.append(float(line[value]))
-        y.append(int(line[1]))
-
-    return x, y
-
-def lineal_regression(values):
-    model = list(map(regression(values), values))
-
+def lineal_regression(y, type):
     plt.figure()
-    plt.rcParams.update({"font.family": "serif"})
-    plt.scatter(values, hours)
-    plt.plot(values, model)
-    plt.savefig("lineal_regression.png")
+    plt.scatter(hours, y)
 
+    m, b = np.polyfit(hours, y, 1)
+    plt.plot(hours, m*np.array(hours)+b)
 
-def regression(values):
-    slope, intercept, r_value, p_value, std_err = stats.linregress(values, hours)
-
-    return slope * values + intercept
+    plt.savefig(f"lineal_regression_{type}.png")
 
 def moving_averages():
     pass
